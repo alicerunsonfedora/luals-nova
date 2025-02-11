e9582a6a5ef2065da327f52465443fbca9e63ccf
Maximilian Litteral <litteral@hey.com>
2025-02-10 22:50:58 -0500

    Fix config not being used

--- Scripts/main.js
+++ Scripts/main.js
@@ -25,7 +25,7 @@ class LuaLanguageServer {
       this
     );
     
-    nova.config.observe(
+    nova.workspace.config.observe(
       "luals.workspace.config-file",
       function (configPath) {
         this.configPath = configPath;
@@ -56,7 +56,7 @@ class LuaLanguageServer {
     };
     
     if (this.configPath) {
-      serverOptions[args] = ["--configfile", this.configPath];
+      serverOptions.args = [`--configpath ${this.configPath}`];
     }
     
     var clientOptions = {
